2b1a0f74e9eee1f185fe6e53d276c636fce5b03d
kwboyd <kwboyd@live.unc.edukwboyd$ git config --global user.email kwboyd@live.unc.edu>
2016-10-14 15:34:45 -0400

    Styled images and buttons

--- js/script.js
+++ js/script.js
@@ -6,31 +6,33 @@ var portfolioHtml = [];
 function getPortfolio() {
     $.getJSON('data/data.json', function(data) {
         $.each(data, function (i, data) {
-                       
-                       if (i % 3 === 0){
-                        portfolioHtml += "<div class='row'>";
-                        appendPortfolio(data);
-                       }
-                        else if (i % 3 === 2) {
-                            appendPortfolio(data);
-                            portfolioHtml += "</div>";
-                        }
-                        else {
-                        appendPortfolio(data);
-                       }
-                       
+                       //
+                       //if (i % 3 === 0){
+                       // portfolioHtml += "<div class='row'>";
+                       // buildPortfolio(data);
+                       //}
+                       // else if (i % 3 === 2) {
+                       //     buildPortfolio(data);
+                       //     portfolioHtml += "</div>";
+                       // }
+                       // else {
+                       // buildPortfolio(data);
+                       //}
+                       //
+                       //
+                       buildPortfolio(data);
                     });
             $("#portfolio-container").append(portfolioHtml);
         })
 }
 
-function appendPortfolio(data) {
-    portfolioHtml += "<div class='portfolio-box col-md-4'>";
+function buildPortfolio(data) {
+    portfolioHtml += "<div class='portfolio-box col-sm-6 col-xl-4'>";
     portfolioHtml += "<a class='img-link' href='" + data.link + "'>"
-    portfolioHtml += "<img class='portfolio-img' src='" + data.image + "' alt='" + data.title + "'></a>";
-    portfolioHtml += "<h3 class='img-title'>" + data.title + "</h3>";
-    portfolioHtml += "<a class='site-link' href='" + data.link + "'> Visit site" + "</a>";
-    portfolioHtml += "<a class='repo-link' href='" + data.repo + "'> Visit repo" + "</a>";
-    portfolioHtml += "<p class='skills'>Skills: " + data.skills + "</p>";
+    portfolioHtml += "<div class='portfolio-img-box'> <img class='portfolio-img' src='" + data.image + "' alt='" + data.title + "'>";
+    portfolioHtml += "<h3 class='img-title'>" + data.title + "</h3></a>";
+    portfolioHtml += "<div id='link-box'><a class='site-link' id='live' href='" + data.link + "'> VISIT SITE" + "</a>";
+    portfolioHtml += "<a class='site-link' id='repo' href='" + data.repo + "'> VISIT REPO" + "</a></div></div>";
+    portfolioHtml += "<p class='skills'><strong>Skills:</strong> " + data.skills + "</p>";
     portfolioHtml += "<p>" + data.description + "</p></div>";
 }

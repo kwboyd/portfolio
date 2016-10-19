fc5939c8426e37c6c64b3f03d47b840695168d09
kwboyd <kwboyd@live.unc.edukwboyd$ git config --global user.email kwboyd@live.unc.edu>
2016-10-18 22:22:21 -0400

    Changed colors

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 $(document).ready(function()  {
     getPortfolio();
     $("#works").click(function() {
-        $("#works-container").show("fast");
-        $("#about-container").hide("fast");
-        $("#resume-container").hide("fast");
+        $("#works-container").fadeIn("fast");
+        $("#about-container").hide();
+        $("#resume-container").hide();
     });
     $("#about").click(function(){
-        $("#works-container").hide("fast");
-        $("#about-container").show("fast");
-        $("#resume-container").hide("fast");
+        $("#works-container").hide();
+        $("#about-container").fadeIn("fast");
+        $("#resume-container").hide();
     })
     $("#contact").click(function(){
-        $("#about-container").hide("fast");
-        $("#works-container").hide("fast");
-        $("#resume-container").show("fast");
+        $("#about-container").hide();
+        $("#works-container").hide();
+        $("#resume-container").fadeIn("fast");
     })
 })
 var portfolioHtml = [];
@@ -24,10 +24,10 @@ function getPortfolio() {
                     portfolioHtml += "<div class='portfolio-box col-sm-6 col-xl-4'>";
                     portfolioHtml += "<a class='img-link' href='" + data.link + "'>"
                     portfolioHtml += "<div class='portfolio-img-box'> <img class='portfolio-img' src='" + data.image + "' alt='" + data.title + "'>";
-                    portfolioHtml += "<h3 class='img-title'>" + data.title + "</h3></a>";
+                    portfolioHtml += "<h3 class='img-title'>" + data.title + "</h3></a></div>";
                     portfolioHtml += "<div id='link-box'><a class='site-link' id='live' href='" + data.link + "'> VISIT SITE" + "</a>";
-                    portfolioHtml += "<a class='site-link' id='repo' href='" + data.repo + "'> VISIT REPO" + "</a></div></div>";
-                    portfolioHtml += "<p class='skills'><strong>Skills:</strong> " + data.skills + "</p>";
+                    portfolioHtml += "<a class='site-link' id='repo' href='" + data.repo + "'> VISIT REPO" + "</a></div>";
+                    portfolioHtml += "<p class='skills'><strong>Skills: " + data.skills + "</strong></p>";
                     portfolioHtml += "<p>" + data.description + "</p></div>";
                     });
             $("#works-container").append(portfolioHtml);
